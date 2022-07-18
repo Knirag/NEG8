f806de7cceefe9c23876b6eb10ef53879f63fd93
Knirag <105249761+Knirag@users.noreply.github.com>
2022-07-18 16:10:04 +0200

    PLD 3

---
+++ myModule.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def greeting(name):
+  print("Hello, " + name)
+  print("CLICK ON THE LINK BELOW TO ACCEPT & READ OUR TERMS OF SERVICE")
+  print("https://www.termsandconditionsgenerator.com/live.php?token=pFepQ6hsJkDzOXvBG0gSok2pNvlZyRBX")
