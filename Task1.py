a3361472cba9ff4bd87c1d9a1d9940a99ce5c20c
Knirag <105249761+Knirag@users.noreply.github.com>
2022-07-21 19:09:44 +0200

    Tsk 1

---
+++ Task1.py
@@ -0,0 +1,11 @@
+
+m = int (input("Input an integer of your choice:"))
+if (m%2) == 0:
+  print("WIERD!")
+else:
+    if m >=2 and m <=5:
+        print("NOT WEIRD")
+    elif m >=6 and m <=20:
+        print("WEIRD")
+    elif m >20:
+        print("NOT WIERD")
